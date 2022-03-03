109932cf92c9393b704e554e00f193c60d66dd5f
Abhinav Sharma <abhinav.161a026@gmail.com>
2022-03-03 13:50:23 +0530

    working on celebrity detail page

---
+++ src/Pages/CelebrityDetail.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+export default function CelebrityDetail(){
+    const id= "621f265b58678b08aa0e0c4e"
+    const [celeb,setCeleb]= useState(null)
+    const [isLoading, setIsLoading]= useState(true)
+}
