c7dd100ee72e085888132d0d78ec34fdf2835550
Pawan Sukhwani <sukhwanipawan@gmail.com>
2022-03-27 00:20:16 +0530

    changes in landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
         <>
             <LandingCarousel />
             <LandingHighlight title='Featured'/>
-            <LandingHighlight title='Bollywoodddddddd'/>
+            <LandingHighlight title='Bollywood'/>
             <LandingHighlight title='Trending'/>
             <LandingHighlight title='Athletes'/>
         </>
